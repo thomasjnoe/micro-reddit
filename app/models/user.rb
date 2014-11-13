23d8cfee4f8ef5e6bf6283ebdd9c6cc6561320a6
Thomas Noe <thomasjnoe@gmail.com>
2014-11-12 22:21:48 -0500

    Add associations for User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 255 }, 
 										uniqueness: { case_sensitive: false },
 										format: { with: VALID_EMAIL_REGEX }
+	has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113000057) do
+ActiveRecord::Schema.define(version: 20141113031912) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
